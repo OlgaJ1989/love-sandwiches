76ad126aa893e79f007733c190abe99b22901930
OlgaJ1989 <olgajasinska89@hotmail.co.uk>
2021-08-26 21:45:35 +0000

    Delete duplicate code

--- run.py
+++ run.py
@@ -1,14 +1,3 @@
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-
-import gspread
-from google.oauth2.service_account import Credentials
-
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
-
 import gspread
 from google.oauth2.service_account import Credentials
 
